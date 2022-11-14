751c4ea4e612732d5ddbe053fd2fc1ce4f8881b2
ahmedhamdy749 <112814634+ahmedhamdy749@users.noreply.github.com>
2022-11-14 04:46:07 +0200

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -105,30 +105,21 @@ class Enrollment(models.Model):
     # Foreign key to lesson
     # question text
     # question grade/mark
-
-    # <HINT> A sample model method to calculate if learner get the score of the question
-    #def is_get_score(self, selected_ids):
-    #    all_answers = self.choice_set.filter(is_correct=True).count()
-    #    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-    #    if all_answers == selected_correct:
-    #        return True
-    #    else:
-    #        return False
-
 class Question(models.Model):
-    GRADE = 1.0
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200, default="question_text")
+    grade = models.IntegerField(default=0)
 
+    def __str___(self):
+        return str(self.question_text)
+    # <HINT> A sample model method to calculate if learner get the score of the question
     def is_get_score(self, selected_ids):
         all_answers = self.choice_set.filter(is_correct=True).count()
         selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
         if all_answers == selected_correct:
             return True
-        return False
-
-
-    lesson_id = models.ForeignKey(Course, on_delete=models.CASCADE)
-    question_text = models.CharField(max_length=500)
-    grade = models.FloatField(default=GRADE)
+        else:
+            return False
 
 
 #  <HINT> Create a Choice Model with:
@@ -138,12 +129,13 @@ class Question(models.Model):
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
 # class Choice(models.Model):
-
 class Choice(models.Model):
-    lesson_id = models.ForeignKey(Course, on_delete=models.CASCADE)
-    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=500, default="Sample text.")
-    is_correct = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200, default="choice_text")
+    is_correct = models.BooleanField(default=False)
+
+    def __str___(self):
+        return f"question: {self.question.question_text}, answer: {self.choice_text}, correct:{self.is_correct}"
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
